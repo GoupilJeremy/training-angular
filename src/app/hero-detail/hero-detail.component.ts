3ff44371998b635f39e52b82b6fff34b884d0d77
goupiljeremy <goupiljeremy@gmail.com>
2018-03-22 12:14:53 +0100

    add a new feature: hero-detail + routing

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
+import { Subscriber } from 'rxjs/Subscriber';
+
 
 @Component({
   selector: 'app-hero-detail',
@@ -9,9 +15,20 @@ import { Hero } from '../hero';
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
 
   ngOnInit() {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+    .subscribe(hero => this.hero = hero);
   }
 
 }
